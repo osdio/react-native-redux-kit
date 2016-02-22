60b19c7ea49b0b6ce4014eb2a46812a47e3414ed
zhanglingyong <zhanglingyong@meituan.com>
2016-02-22 11:55:07 +0800

    change base services and add config

--- src/services/request.js
+++ src/services/request.js
@@ -1,25 +1,51 @@
-import queryString from 'query-string';
+import qs from 'query-string';
+import config from '../configs';
+
+
+const urlPrefix = config.domain + config.apiPath;
+
+
+function filterJSON(res) {
+	return res.json();
+}
+
+
+function filterStatus(res) {
+	if (res.status >= 200 && res.status < 300) {
+		return res
+	}
+	else {
+		let error = new Error(res.statusText);
+		error.res = res;
+		error.type = 'http';
+		throw error;
+	}
+}
 
 
 export function get(url, params) {
-    if (params) {
-        url += '?' + queryString.stringify(params);
-    }
-    return fetch(url)
-        .then(res=>res.json());
+	url = urlPrefix + url;
+	if (params) {
+		url += `?${qs.stringify(params)}`;
+	}
+
+	return fetch(url)
+		.then(filterStatus)
+		.then(filterJSON);
 }
 
 
 export function post(url, body) {
-    let fetchOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    };
-
-    return fetch(url, fetchOptions)
-        .then(res=>res.json());
+	return fetch(urlPrefix + url, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	})
+		.then(filterStatus)
+		.then(filterJSON);
 }
+
+
--- src/services/storage.js
+++ src/services/storage.js
@@ -1,37 +1,38 @@
 import React,{
-    AsyncStorage
+	AsyncStorage
 } from 'react-native';
 
 
-export const removeItem = AsyncStorage.removeItem;
+export function setItem(key, value) {
+	if (value == null) return Promise.reject('StorageService Error: value should not be null or undefined');
+	return AsyncStorage.setItem(key, JSON.stringify(value));
+}
+
+
+export function getItem(key) {
+	return AsyncStorage.getItem(key)
+		.then(function (value) {
+			return JSON.parse(value)
+		});
+}
 
 
 export const clear = AsyncStorage.clear;
 
 
-export function setItem(key, value) {
-    if (value == null) return Promise.reject('value is null');
-    return AsyncStorage.setItem(key, JSON.stringify(value));
-}
-
-
-export function getItem(key) {
-    return AsyncStorage.getItem(key)
-        .then(function (value) {
-            return JSON.parse(value)
-        });
-}
+export const removeItem = AsyncStorage.removeItem;
 
 
 export function multiGet(keys) {
-    return AsyncStorage.multiGet(keys)
-        .then(results=> {
-            return results.map(item=> {
-                return [item[0], JSON.parse(item[1])]
-            });
-        });
+	return AsyncStorage.multiGet(keys)
+		.then(results=> {
+			return results.map(item=> {
+				return [item[0], JSON.parse(item[1])]
+			})
+		});
 }
 
+
 export function multiRemove(keys) {
-    return AsyncStorage.multiRemove(keys);
+	return AsyncStorage.multiRemove(keys)
 }
